8f3eed0661ea028922ebb5c147f167689f66dc6d
zzl <863593420@qq.com>
2019-09-16 11:54:57 +0800

    update zzl 05 yysdlmapper

--- weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/YysdlMapper.java
+++ weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/YysdlMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface YysdlMapper {
     //运行商登录
-    public Yysdl getloginYysdl(Yysdl yysdl);
+    public Yysdl getloginYysdl(Yysdl yysdl)throws Exception;
 
 
 }
