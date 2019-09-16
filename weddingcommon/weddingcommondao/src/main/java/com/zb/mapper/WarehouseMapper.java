08f28974f077777f1ee6e7571dc0daf4fefdbfac
zzl <863593420@qq.com>
2019-09-16 16:17:28 +0800

    update zzl 0

--- weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/WarehouseMapper.java
+++ weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/WarehouseMapper.java
@@ -7,15 +7,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface WarehouseMapper {
-
+	//唯一查
 	public Warehouse getWarehouseById(@Param(value = "id") Integer id)throws Exception;
-
+     //全部查
 	public List<Warehouse>	getWarehouseListByMap(Map<String,Object> param)throws Exception;
-
+   //查次数
 	public Integer getWarehouseCountByMap(Map<String,Object> param)throws Exception;
-
+    //添加
 	public Integer insertWarehouse(Warehouse warehouse)throws Exception;
-
+    //修改
 	public Integer updateWarehouse(Warehouse warehouse)throws Exception;
 
 	public Integer deleteWarehouseById(@Param(value = "id") Integer id)throws Exception;
