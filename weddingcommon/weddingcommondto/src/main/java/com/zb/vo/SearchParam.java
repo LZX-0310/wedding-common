0f2ec4e57ae213edefb99b36fd3325ef04c7a1e4
qingang <734654628@qq.com>
2019-09-26 17:25:18 +0800

    qg49

--- weddingcommon/weddingcommondto/src/main/java/com/zb/vo/SearchParam.java
+++ weddingcommon/weddingcommondto/src/main/java/com/zb/vo/SearchParam.java
@@ -3,6 +3,8 @@ package com.zb.vo;
 public class SearchParam {
     private String key;
     private Integer xtypeid;
+    private Integer index;
+    private Integer size;
 
     public String getKey() {
         return key;
@@ -18,5 +20,21 @@ public class SearchParam {
 
     public void setXtypeid(Integer xtypeid) {
         this.xtypeid = xtypeid;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
     }
 }
