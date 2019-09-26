5013c83d4ba4c6cd64f677cafc0d7eeb8ff4aea6
qingang <734654628@qq.com>
2019-09-26 17:24:10 +0800

    qg48

---
+++ weddingcommon/weddingcommondto/src/main/java/com/zb/vo/SearchParam.java
@@ -0,0 +1,22 @@
+package com.zb.vo;
+
+public class SearchParam {
+    private String key;
+    private Integer xtypeid;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Integer getXtypeid() {
+        return xtypeid;
+    }
+
+    public void setXtypeid(Integer xtypeid) {
+        this.xtypeid = xtypeid;
+    }
+}
