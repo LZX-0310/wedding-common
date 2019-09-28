064d08ab1be7e7027c7bd9eaff5a54e215f3c94c
zzl <863593420@qq.com>
2019-09-28 13:57:52 +0800

    update zzl 29

--- weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/JeMapper.java
+++ weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/JeMapper.java
@@ -7,5 +7,8 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 @Mapper
 public interface JeMapper {
+    //查询待支付和已支付
     public JeVo getje(@Param("state") Integer state)throws Exception;
+    //查询退款总金额
+    public  JeVo getTuikuanje(@Param("state") Integer state)throws Exception;
 }
