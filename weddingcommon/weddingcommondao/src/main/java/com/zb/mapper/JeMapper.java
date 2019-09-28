5caf156e60a4296a4a7d4eefb7321ebae9798440
zzl <863593420@qq.com>
2019-09-28 13:09:40 +0800

    update zzl 28

--- weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/JeMapper.java
+++ weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/JeMapper.java
@@ -1,10 +1,11 @@
 package com.zb.mapper;
 
+import com.zb.vo.JeVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 @Mapper
 public interface JeMapper {
-    public  Integer getje(@Param("state") Integer state)throws Exception;
+    public JeVo getje(@Param("state") Integer state)throws Exception;
 }
