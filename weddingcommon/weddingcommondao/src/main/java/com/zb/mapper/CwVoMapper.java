16996d756b869f4807d194daa7a88ce97ad5a221
zzl <863593420@qq.com>
2019-09-29 17:53:33 +0800

    update zzl 37

--- weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/CwVoMapper.java
+++ weddingcommon/weddingcommondao/src/main/java/com/zb/mapper/CwVoMapper.java
@@ -1,12 +1,13 @@
 package com.zb.mapper;
 
+import com.zb.vo.CwVo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
 
 @Mapper
-public interface CwVo {
+public interface CwVoMapper {
     public List<CwVo>FindCwVoAll(Map<String,Object>map)throws Exception;
 
     public  int getCwVoCount(Map<String,Object>map)throws Exception;
