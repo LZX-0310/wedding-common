5c3e4a82c740267e572a33d848c7ebd087e3b01c
qingang <734654628@qq.com>
2019-10-07 08:31:55 +0800

    qg84

--- weddingcommon/weddingcommonmodule/src/main/java/com/zb/pojo/Tmorder.java
+++ weddingcommon/weddingcommonmodule/src/main/java/com/zb/pojo/Tmorder.java
@@ -12,7 +12,7 @@ public class Tmorder implements Serializable {
     //商品数量
     private Integer num;
     //引用订单id表(如果是已付款必须有订单编号)
-    private Integer orderid;
+    private String orderid;
     //1未付款2已付款
     private Integer state;
     //引用商品id
@@ -51,10 +51,10 @@ public class Tmorder implements Serializable {
     public  Integer getNum(){
         return this.num;
     }
-    public void setOrderid (Integer  orderid){
+    public void setOrderid (String  orderid){
         this.orderid=orderid;
     }
-    public  Integer getOrderid(){
+    public  String getOrderid(){
         return this.orderid;
     }
     public void setState (Integer  state){
